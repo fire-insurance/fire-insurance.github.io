5d2f272846faead7e3226851fe937ebf39193a62
Dmitry Duda <dmitrij.snake2000@yandex.ru>
2022-05-24 17:08:53 +1000

    header fixes & 'about' block

--- Projects/GUZEL/main.js
+++ Projects/GUZEL/main.js
@@ -1,16 +1,20 @@
 $(() => {
+
+    // Обработка нажатия на бургер или затемнение экрана под модальной навигацией
+
     let burger = $('.burger');
     let modalNavigation = $('.modal-menu');
     let darkenBackground = $('.darkening');
-    burger.click(() => {
-        burger.toggleClass('burger_opened')
-        modalNavigation.toggleClass('modal-menu_opened')
-        darkenBackground.toggleClass('darkening_active')
-    })
+    let header = $('.header');
 
-    darkenBackground.click(() => {
+    const toggleBurger = () => {
         burger.toggleClass('burger_opened')
         modalNavigation.toggleClass('modal-menu_opened')
         darkenBackground.toggleClass('darkening_active')
-    } )
+        header.toggleClass('header_modal-opened')
+    }
+
+    burger.click(() => toggleBurger())
+    darkenBackground.click(() => toggleBurger())
+
 });
