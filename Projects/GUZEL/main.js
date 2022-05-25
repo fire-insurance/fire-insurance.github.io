3bd6a06457e9a2779adcfb84c56cf506c98fea0b
Dmitry Duda <dmitrij.snake2000@yandex.ru>
2022-05-25 21:30:25 +1000

    exp slider

--- Projects/GUZEL/main.js
+++ Projects/GUZEL/main.js
@@ -1,4 +1,4 @@
-$(() => {
+$(document).ready(() => {
 
     // Обработка нажатия на бургер или затемнение экрана под модальной навигацией
 
@@ -14,7 +14,31 @@ $(() => {
         header.toggleClass('header_modal-opened')
     }
 
-    burger.click(() => toggleBurger())
-    darkenBackground.click(() => toggleBurger())
+    burger.on('click', () => toggleBurger())
+    darkenBackground.on('click', () => toggleBurger())
+
+    // Слайдер с образованием и опытом работы
+
+    var swiper = new Swiper(".educ-swiper-slides", {
+        slidesPerView: 1.7,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        breakpoints: {
+            460: {
+                slidesPerView: 2.5
+            },
+            768: {
+                slidesPerView: 3
+            }
+        }
+    });
 
 });
